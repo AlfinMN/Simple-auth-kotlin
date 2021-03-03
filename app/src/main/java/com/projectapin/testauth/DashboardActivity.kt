ef542ddcb18a3e64c9bacf57c7d75cd9f94b1410
AlfinMN <alfinmuhammadnurdin@gmail.com>
2021-03-03 14:32:09 +0700

    update banner

--- app/src/main/java/com/projectapin/testauth/DashboardActivity.kt
+++ app/src/main/java/com/projectapin/testauth/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.projectapin.testauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
